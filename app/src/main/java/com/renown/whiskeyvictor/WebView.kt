4635fa7666b166c173637443a8ac82d85a11b88a
shaunaqpaul <shaunaqpaulofficial@gmail.com>
2024-06-28 12:12:00 +0530

    Enable clipboard access and fix scrolling in existing sessions recycler view.

--- app/src/main/java/com/renown/whiskeyvictor/WebView.kt
+++ app/src/main/java/com/renown/whiskeyvictor/WebView.kt
@@ -48,6 +48,7 @@ class WebView : AppCompatActivity() {
             webViewMain.webViewClient = WebViewClient()
             webViewMain.webChromeClient = WebChromeClient()
             webViewMain.settings.javaScriptEnabled = true
+            webViewMain.settings.domStorageEnabled = true
             webViewMain.settings.allowFileAccess = true
             webViewMain.settings.userAgentString = "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
             webViewMain.loadUrl(url)
@@ -62,6 +63,7 @@ class WebView : AppCompatActivity() {
             webViewMain.webViewClient = WebViewClient()
             webViewMain.webChromeClient = WebChromeClient()
             webViewMain.settings.javaScriptEnabled = true
+            webViewMain.settings.domStorageEnabled = true
             webViewMain.settings.allowFileAccess = true
             webViewMain.settings.userAgentString = "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
             webViewMain.loadUrl(existingSession.url)
